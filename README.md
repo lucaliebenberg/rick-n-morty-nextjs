980ffc7243e7113373d540dd7f89e2989a1f2ef1
Luca Liebenberg <liebenbergluca@gmail.com>
2024-06-06 16:58:22 +0200

    added pagination + updated styling & color

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Rick n Morty Tinkering 
+This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
 
--- app/characters/page.tsx
+++ app/characters/page.tsx
@@ -1,14 +1,23 @@
 import {getCharacters} from "@/app/api/getCharacters";
 import Image from "next/image";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination"
+
 
 export default async function CharactersPage() {
   const returnedData = await getCharacters()
-  // console.log('returned data --> ', returnedData)
   const characters = returnedData.results
   console.log('characters --> ', characters)
 
   return (
-    <div className={"h-full"}>
+    <div className={"h-full pb-8"}>
       <div className={"flex justify-center align-middle"}>
         <div className="pt-2 relative mx-auto text-gray-600">
           <input className="border-2 border-gray-300 bg-white h-10 px-8 pr-16 rounded-lg text-sm focus:outline-none"
@@ -17,7 +26,7 @@ export default async function CharactersPage() {
           </button>
         </div>
       </div>
-      <div className={"p-2 grid grid-cols-4 gap-6"}>
+      <div className={"p-2 grid grid-cols-4 gap-6 pb-12"}>
         {characters.map((character: any) => (
           <div key={character.id}>
             <h1 className={"text-[1rem] font-bold"}>{character.name}</h1>
@@ -25,6 +34,32 @@ export default async function CharactersPage() {
           </div>
         ))}
       </div>
+
+      {/* Handle pagination */}
+      <Pagination>
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious href="#"/>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">1</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#" isActive className={"text-black"}>
+              2
+            </PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">3</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationEllipsis/>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext href="#"/>
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
     </div>
   )
 }
